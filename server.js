fea88c3be075af841210924ef638d7c741f6fda2
Victor Mimo <victormimoc@gmail.com>
2023-01-30 19:55:50 -0500

    updating server to OCR first page, classify province, send to FR

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ require('dotenv').config()
 
 const documentSplitter = require('./documentSplitter')
 const runOCR = require('./ocr/runOCR')
+const classifyProvince = require('./utils/classifyProvince')
+const runFR = require('./formRecognizer/runFR')
+const formatData = require('./format')
+const bcData = require('./lifeLabsFrResult.json')
 
 const app = express()
 const port = 3000
@@ -18,25 +22,55 @@ app.get('/', (_, res) => {
 })
 
 app.post('/analyze', async (req, res) => {
-  const { pdf, companyName } = req.body
+  // const { pdf, companyName } = req.body
 
-  res.status(200).send('Analyzing...')
+  // res.status(200).send('Analyzing...')
 
-  console.log({ companyName })
-  console.log('pdf as base64 ', pdf.slice(0, 50))
+  // console.log({ companyName })
+  // console.log('pdf as base64 ', pdf.slice(0, 50))
 
-  const buffer = Buffer.from(pdf, 'base64')
+  // const buffer = Buffer.from(pdf, 'base64')
 
-  console.log('pdf as buffer ', buffer)
+  // console.log('pdf as buffer ', buffer)
 
-  const { pages, numberOfPages } = await documentSplitter(buffer)
+  // const { pages, numberOfPages } = await documentSplitter(buffer)
 
-  console.log({ pages })
-  console.log({ numberOfPages })
+  // console.log('splitting PDF into pages...')
+  // console.log({ pages })
+  // console.log({ numberOfPages })
 
-  const ocrPages = await runOCR(pages)
+  // // TODO: refactor to a cleaner way
+  // const newArr = []
+  // newArr.push(pages[0])
 
-  console.log({ ocrPages })
+  // console.log('first page arr, ', newArr)
+
+  // // run OCR on the first page
+  // const firstPageOCR = await runOCR(newArr)
+
+  // console.log('running OCR on first page...')
+  // console.log({ firstPageOCR })
+
+  // const province = classifyProvince(firstPageOCR[0])
+
+  // console.log('province is ', province)
+
+  // // experiment: pass the entire document to FR and see what happens
+  // const data = await runFR(buffer, province)
+
+  // console.log('Finished FR')
+
+  // const formattedData = formatData(data, province)
+
+  // COMMENT OUT THE ABOVE when creating the formatted data structure.
+  // bcData
+
+  console.log('formatted data', bcData)
+
+  // 3 main features:
+  // 1. use the text result to determine which province the doc is from
+  // 2. send the complete PDF to the respective province model on form recognizer
+  // 3. extract the form recognizer results and structure it into a JSON object
 
   // send this back to a url provided
 })
