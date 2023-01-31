d0f0f9978ef42a519e2ed657e5ad5e5c4d0b1491
Victor Mimo <victormimoc@gmail.com>
2023-01-30 19:54:53 -0500

    adding helper function to classify each doc per province

---
+++ utils/classifyProvince.js
@@ -0,0 +1,21 @@
+const PROVINCES = require('../utils/enums').PROVINCES
+
+const classifyProvince = (document) => {
+  console.log('document inside  ', document)
+
+  const isDocumentFromBC = document.includes('BC Company Summary')
+  const isDocumentFromAlberta = document.includes(
+    'Government Corporation/Non-Profit Search of Alberta',
+  )
+  const isDocumentFromOntario = document.includes('ONTARIO_PLACEHOLDER')
+
+  if (isDocumentFromBC) {
+    return PROVINCES.BC
+  } else if (isDocumentFromAlberta) {
+    return PROVINCES.AB
+  } else if (isDocumentFromOntario) {
+    return PROVINCES.ON
+  }
+}
+
+module.exports = classifyProvince
