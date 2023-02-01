74ebeedbfb7d392888bba014e8d67e5e9c4cd759
David Gomez <dgomez1296@gmail.com>
2023-02-01 18:53:57 -0500

    Removed COLUMN2 as a conditional + commented in possible refactoring

--- format/helperFunctions.js
+++ format/helperFunctions.js
@@ -5,15 +5,14 @@ exports.dynamicKeyValuePairsHelper = (extractedObject, arrayToExtract) => {
     //For this property we want to assign key-value pairs based on the "KEY" key and "VALUE" key
     if (properties.VALUE) {
       extractedObject[properties.KEY.value] = properties.VALUE.value || "";
-    } else if (properties.COLUMN2) {
-      extractedObject[properties.KEY.value] = properties.COLUMN2.value || "";
-    } else {
+    }  else {
       extractedObject[properties.KEY.value] = "";
     }
   }
 };
 
 exports.staticKeyPairsHelper = (extractedObject, arrayToExtract) => {
+  
   for (let i = 0; i < arrayToExtract.length; i++) {
     const properties = arrayToExtract[i].properties;
 
@@ -21,9 +20,34 @@ exports.staticKeyPairsHelper = (extractedObject, arrayToExtract) => {
       extractedObject[key] = properties[key].value || "";
     }
   }
+
+  //Refactored to make dryer. This code assumes the values array will only have one object
+    /*
+    const properties = arrayToExtract[0].properties;
+
+    for (const key in properties) {
+      extractedObject[key] = properties[key].value || "";
+    }
+    */
+
+  // Refactored to make dryer. For the following code remove 'extracedObject' param
+  // This method runs a reduce method on the input "values" array setting an empty object as the initial value
+      /*
+      const extractedValues = arrayToExtract.reduce((acc, curr) => {
+      Object.keys(curr.properties).forEach(key => {
+        acc[key] = curr.properties[key].value;
+      });
+      return acc
+      }, {})
+      console.log(extractedValues)
+      return extractedValues 
+     */
+
+
 };
 
 exports.arrayOfObjectsHelper = (extractedObject, arrayToExtract) => {
+  
   for (let i = 0; i < arrayToExtract.length; i++) {
     const helperObject = {};
 
@@ -33,4 +57,15 @@ exports.arrayOfObjectsHelper = (extractedObject, arrayToExtract) => {
     }
     extractedObject.push(helperObject);
   }
+
+  //Refactored to try to reduce time complexity. 
+  // for (let i = 0; i < arrayToExtract.length; i++) {
+  //   const properties = arrayToExtract[i].properties;
+  //   const helperObject = Object.keys(properties).reduce((acc, key) => {
+  //     acc[key] = properties[key].value || "";
+  //     return acc;
+  //   }, {});
+  //   extractedObject.push(helperObject);
+  // }
+
 };
