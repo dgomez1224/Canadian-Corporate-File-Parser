b3b1c5f25ff38320b07f1bc672a9f974ba4545b6
David Gomez <dgomez1296@gmail.com>
2023-02-02 13:23:59 -0500

    Cleaned up staticKeyPairsHelper

--- format/helperFunctions.js
+++ format/helperFunctions.js
@@ -13,35 +13,16 @@ exports.dynamicKeyValuePairsHelper = (extractedObject, arrayToExtract) => {
 
 exports.staticKeyPairsHelper = (extractedObject, arrayToExtract) => {
   
-  for (let i = 0; i < arrayToExtract.length; i++) {
-    const properties = arrayToExtract[i].properties;
-
-    for (const key in properties) {
-      extractedObject[key] = properties[key].value || "";
-    }
-  }
+  
 
   //Refactored to make dryer. This code assumes the values array will only have one object
-    /*
+    
     const properties = arrayToExtract[0].properties;
 
     for (const key in properties) {
       extractedObject[key] = properties[key].value || "";
     }
-    */
-
-  // Refactored to make dryer. For the following code remove 'extracedObject' param
-  // This method runs a reduce method on the input "values" array setting an empty object as the initial value
-      /*
-      const extractedValues = arrayToExtract.reduce((acc, curr) => {
-      Object.keys(curr.properties).forEach(key => {
-        acc[key] = curr.properties[key].value;
-      });
-      return acc
-      }, {})
-      console.log(extractedValues)
-      return extractedValues 
-     */
+    
 
 
 };
