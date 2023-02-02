982e33b2ab2d78a172e02380c9904d105c6f32e4
Victor Mimo <victormimoc@gmail.com>
2023-02-01 19:47:08 -0500

    adding template for provinces

--- utils/enums.js
+++ utils/enums.js
@@ -3,3 +3,21 @@ exports.PROVINCES = {
   BC: 'BRITISH_COLUMBIA',
   ON: 'ONTARIO',
 }
+
+/*
+  AB = "AB",
+    BC = "BC",
+    MB = "MB",
+    NB = "NB",
+    NL = "NL",
+    NS = "NS",
+    NT = "NT",
+    NU = "NU",
+    ON = "ON",
+    PE = "PE",
+    QC = "QC",
+    SK = "SK",
+    YT = "YT",
+
+
+*/
