6a6faacaca0ae10bb3664b661926300a1e797371
David Gomez <dgomez1296@gmail.com>
2023-02-09 15:07:56 -0500

    Added comments

--- bot/federalCorpBot.js
+++ bot/federalCorpBot.js
@@ -15,16 +15,16 @@ const federalCorpBot = async (companyName) => {
 
   await page.goto(navigateTo);
 
-  //   await page.waitForNavigation();
 
   await page.waitForSelector("#corpName");
 
+  //Sets the download path for the pdf
   await page._client().send("Page.setDownloadBehavior", {
     behavior: "allow",
     downloadPath: downloadPath,
   });
 
-
+ //Fills in the Corporate Name field using the POST value
   await page.$eval(
     "#corpName",
     (el, companyName) => (el.value = companyName),
@@ -33,6 +33,7 @@ const federalCorpBot = async (companyName) => {
 
   await page.click("#buttonNext");
 
+  //Waits for the results to show up and clicks the first one
   await page.waitForSelector(
     "#tables > section > section > ol > li > div > div.col-md-11 > span:nth-child(1) > a"
   );
@@ -40,6 +41,7 @@ const federalCorpBot = async (companyName) => {
     "#tables > section > section > ol > li > div > div.col-md-11 > span:nth-child(1) > a"
   );
 
+ //Waits for the next page to load and clicks the "order" link
   await page.waitForSelector(
     "#metrics > div > div > div > section > div.clearBoth.ic_info > a:nth-child(2)"
   );
@@ -47,6 +49,7 @@ const federalCorpBot = async (companyName) => {
     "#metrics > div > div > div > section > div.clearBoth.ic_info > a:nth-child(2)"
   );
 
+  //Waits for the next page to load and inputs sample email taken from .env
   await page.waitForSelector('input[id="request.contact.emailAddress"]');
   await page.$eval(
     'input[id="request.contact.emailAddress"]',
@@ -60,6 +63,7 @@ const federalCorpBot = async (companyName) => {
     emailAddress
   );
 
+  //Needs to wait half a second before clicking or else the click wont regist
   await new Promise((resolve) => setTimeout(resolve, 500));
 
   await page.waitForSelector("#buttonNext");
@@ -67,6 +71,7 @@ const federalCorpBot = async (companyName) => {
   await page.waitForSelector("#finish");
   await page.click("#finish");
 
+  //Waits for the page to load and clicks "Download PDF"
   await page.waitForSelector(
     "#tables > section > div.noteBoxB.margin40.padding20 > table > tbody > tr > td:nth-child(2) > strong > a:nth-child(2)"
   );
